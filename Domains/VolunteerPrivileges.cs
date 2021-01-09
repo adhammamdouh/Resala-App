9fa2a121a894e09c511d45778d74de7892b3d5e7
mohamedmostafaouda <55032769+mohamedmostafaouda@users.noreply.github.com>
2021-01-09 18:04:50 +0200

    Domains Added

--- Domains/VolunteerPrivileges.cs
+++ Domains/VolunteerPrivileges.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Resala.App.CRM.Domains
+namespace Resala_App.Domains
 {
     public class VolunteerPrivileges
     {
